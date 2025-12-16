e120be2ed5ca4b4ec3fd40a5fb99efb1c6fb2ad6
r021n <rozinabdul25@gmail.com>
2025-12-16 21:19:16 +0700

    feat: Add subscription creation endpoint, controller, and validation.

--- artisancrate/backend/src/validators/subscriptionValidators.ts
+++ artisancrate/backend/src/validators/subscriptionValidators.ts
@@ -11,20 +11,20 @@ export const createSubscriptionSchema = z.object({
       })
       .int()
       .positive("subscription_plan_id harus > 0"),
-  }),
-  shipping_address_id: z
-    .number({
+    shipping_address_id: z
+      .number({
+        error: (iss) =>
+          iss.input === undefined
+            ? "shipping_address_id wajib diisi"
+            : "shipping_address_id harus angka",
+      })
+      .int()
+      .positive("shipping_address_id harus > 0"),
+    payment_method_type: z.enum(["manual_payment_link", "credit_card_token"], {
       error: (iss) =>
         iss.input === undefined
-          ? "shipping_address_id wajib diisi"
-          : "shipping_address_id harus angka",
-    })
-    .int()
-    .positive("shipping_address_id harus > 0"),
-  payment_method_type: z.enum(["manual_payment_link", "credit_card_token"], {
-    error: (iss) =>
-      iss.input === undefined
-        ? "payment_method_type wajib diisi"
-        : "payment_method_type haru salah satu: 'manual_payment_link' | 'credit_card_token'",
+          ? "payment_method_type wajib diisi"
+          : "payment_method_type haru salah satu: 'manual_payment_link' | 'credit_card_token'",
+    }),
   }),
 });
